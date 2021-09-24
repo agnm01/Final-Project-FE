d2b9a20893e6bb4cca16e563fd49650a1b1abf14
parpa <parpa@DESKTOP-1JUTT8T>
2021-09-24 10:09:07 +0300

    Login form added to the component

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="center">
+      <h1>Login</h1>
+      <form method="post">
+        <div class="txt_field">
+          <input type="text" required>
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class="pass">Forgot Password?</div>
+        <input type="submit" value="Login">
+        <div class="signup_link">
+          Not a member? Too bad :(
+        </div>
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
