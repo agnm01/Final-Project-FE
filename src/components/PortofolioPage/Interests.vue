b643ad1c06903872046dc3ac84da668a0e62673e
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-27 02:11:00 +0300

    Updated Interests and implemented Resume component with BE

--- src/components/PortofolioPage/Interests.vue
+++ src/components/PortofolioPage/Interests.vue
@@ -14,73 +14,73 @@
             <div class="col-lg-3 col-md-4">
               <div class="icon-box">
                 <i class="bx bx-ball" style="color: #ffbb2c"></i>
-                <h3>Lorem Ipsum</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
               <div class="icon-box">
                 <i class="bx bx-abacus" style="color: #5578ff"></i>
-                <h3>Dolor Sitema</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
               <div class="icon-box">
                 <i class="bx bx-album" style="color: #e80368"></i>
-                <h3>Sed perspiciatis</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
               <div class="icon-box">
                 <i class="bx bx-paint" style="color: #e361ff"></i>
-                <h3>Magni Dolores</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-planet" style="color: #47aeff"></i>
-                <h3>Nemo Enim</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-car" style="color: #ffa76e"></i>
-                <h3>Eiusmod Tempor</h3>
+                <h3>{{ applicationState.userInfo.interests[1] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-cloud" style="color: #11dbcf"></i>
-                <h3>Midela Teren</h3>
+                <h3>{{ applicationState.userInfo.interests[2] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-cycling" style="color: #4233ff"></i>
-                <h3>Pira Neve</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-anchor" style="color: #b2904f"></i>
-                <h3>Dirada Pack</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-aperture" style="color: #b20969"></i>
-                <h3>Moton Ideal</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-run" style="color: #ff5828"></i>
-                <h3>Verdo Park</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
             <div class="col-lg-3 col-md-4 mt-4">
               <div class="icon-box">
                 <i class="bx bx-book" style="color: #29cc61"></i>
-                <h3>Flavor Nivelanda</h3>
+                <h3>{{ applicationState.userInfo.interests[0] }}</h3>
               </div>
             </div>
           </div>
