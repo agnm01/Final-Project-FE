b0e5d4435a5482c22dd6bbdc02d812a240917e76
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-26 22:59:19 +0300

    Changed to webrouter html5 Linked the header links to section ids

--- src/components/PortofolioPage/Header.vue
+++ src/components/PortofolioPage/Header.vue
@@ -4,43 +4,43 @@
     <nav id="navbar" class="navbar nav-menu">
       <ul>
         <li>
-          <a href="#/home" class="nav-link scrollto active"
+          <a href="#hero" class="nav-link scrollto active"
             ><i class="bx bx-home"></i> <span>Home</span></a
           >
         </li>
 
         <li>
-          <a href="#/about" class="nav-link scrollto"
+          <a href="#about" class="nav-link scrollto"
             ><i class="bx bx-user"></i> <span>About</span></a
           >
         </li>
 
         <li>
-          <a href="#/skills" class="nav-link scrollto"
+          <a href="#skills" class="nav-link scrollto"
             ><i class="bx bx-code"></i> <span>Skills</span></a
           >
         </li>
 
         <li>
-          <a href="#/interests" class="nav-link scrollto"
+          <a href="#interests" class="nav-link scrollto"
             ><i class="bx bx-camera"></i> <span>Interests</span></a
           >
         </li>
 
         <li>
-          <a href="#/resume" class="nav-link scrollto"
+          <a href="#resume" class="nav-link scrollto"
             ><i class="bx bx-file-blank"></i> <span>Resume</span></a
           >
         </li>
 
         <li>
-          <a href="#/services" class="nav-link scrollto"
+          <a href="#services" class="nav-link scrollto"
             ><i class="bx bx-terminal"></i> <span>Tools & Technologies</span></a
           >
         </li>
 
         <li>
-          <a href="#/teammates" class="nav-link scrollto"
+          <a href="#teammates" class="nav-link scrollto"
             ><i class="bx bx-group"></i> <span>Team Members</span></a
           >
         </li>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Portofolio from "../views/Portofolio.vue";
 
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
