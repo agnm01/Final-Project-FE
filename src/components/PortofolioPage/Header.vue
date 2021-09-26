871c08f5aed5887cd28ab7db7dd27c70cc6a18fa
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-26 16:34:18 +0300

    changed design, added profile picture in about section

--- src/components/PortofolioPage/Header.vue
+++ src/components/PortofolioPage/Header.vue
@@ -1,4 +1,5 @@
 <template>
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
   <header id="header" class="d-flex flex-column justify-content-center">
     <nav id="navbar" class="navbar nav-menu">
       <ul>
