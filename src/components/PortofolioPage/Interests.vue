d61a291182e3df179ed986a10c713cecedd422bf
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-27 01:58:49 +0300

    Updated Interests component

--- src/components/PortofolioPage/Interests.vue
+++ src/components/PortofolioPage/Interests.vue
@@ -5,10 +5,7 @@
         <div class="section-title">
           <h2>Interests</h2>
           <p>
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-            aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-            quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-            fugiat sit in iste officiis commodi quidem hic quas.
+            {{ applicationState.userInfo.description }}
           </p>
         </div>
 
@@ -94,5 +91,13 @@
 </template>
 
 <script>
-export default {};
+import { applicationState } from "../../state";
+
+export default {
+  data() {
+    return {
+      applicationState,
+    };
+  },
+};
 </script>
